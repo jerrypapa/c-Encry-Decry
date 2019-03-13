2ad13f5d20b8d382af7758b8b696b644439f469e
Jerry Brad <jbrad@fintech-group.com>
2019-03-13 12:36:54 +0300

    Added Encry/Decry

---
+++ Encryp_test/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Encryp_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string enc=  Encry_Deprcy.Encrypt("igunza@gmail.com", "fincapture");
+          //  Console.WriteLine("encrypted data is: "+enc);
+            string dec = Encry_Deprcy.Decrypt(enc, "fincapture");
+            //string dec2 = Encry_Deprcy.Decrypt("hmCiiA3wEW3fXEfhsVM7Ag==", "papa");
+            //Console.WriteLine("decrypted data is: "+dec);
+            //Console.ReadKey();
+
+           // ...........
+            FileStream ostrm;
+            StreamWriter writer;
+            TextWriter oldOut = Console.Out;
+            try
+            {
+                ostrm = new FileStream("C:/Users/jbrand/Documents/encr_results2/Encryption_Results4.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot open Redirect.txt for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+            Console.SetOut(writer);
+            Console.WriteLine(enc);
+            Console.WriteLine(dec);
+            //Console.WriteLine(dec2+":latest");
+            Console.WriteLine("these are the results jerry");
+            Console.SetOut(oldOut);
+            writer.Close();
+            ostrm.Close();
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+
+    }
+        public class Encry_Deprcy
+        {
+            public static string Decrypt(string textToDecrypt, string key)
+            {
+                RijndaelManaged rijndaelCipher = new RijndaelManaged();
+                rijndaelCipher.Mode = CipherMode.CBC;
+                rijndaelCipher.Padding = PaddingMode.PKCS7;
+
+            rijndaelCipher.KeySize = 0x80;
+                rijndaelCipher.BlockSize = 0x80;
+                byte[] encryptedData = Convert.FromBase64String(textToDecrypt);
+                byte[] pwdBytes = Encoding.UTF8.GetBytes(key)
+    ;
+                byte[] keyBytes = new byte[0x10];
+                int len = pwdBytes.Length;
+                if (len > keyBytes.Length)
+                {
+                    len = keyBytes.Length;
+                }
+                Array.Copy(pwdBytes, keyBytes, len);
+                rijndaelCipher.Key = keyBytes;
+                rijndaelCipher.IV = keyBytes;
+                byte[] plainText = rijndaelCipher.CreateDecryptor().TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+                return Encoding.UTF8.GetString(plainText);
+            }
+
+            public static string Encrypt(string textToEncrypt, string key)
+            {
+                RijndaelManaged rijndaelCipher = new RijndaelManaged();
+                rijndaelCipher.Mode = CipherMode.CBC;
+                rijndaelCipher.Padding = PaddingMode.PKCS7;
+
+            rijndaelCipher.KeySize = 0x80;
+                rijndaelCipher.BlockSize = 0x80;
+                byte[] pwdBytes = Encoding.UTF8.GetBytes(key)
+    ;
+                byte[] keyBytes = new byte[0x10];
+                int len = pwdBytes.Length;
+                if (len > keyBytes.Length)
+                {
+                    len = keyBytes.Length;
+                }
+                Array.Copy(pwdBytes, keyBytes, len);
+                rijndaelCipher.Key = keyBytes;
+                rijndaelCipher.IV = keyBytes;
+                ICryptoTransform transform = rijndaelCipher.CreateEncryptor();
+                byte[] plainText = Encoding.UTF8.GetBytes(textToEncrypt);
+                return Convert.ToBase64String(transform.TransformFinalBlock(plainText, 0, plainText.Length));
+            }
+        }
+    }
+
